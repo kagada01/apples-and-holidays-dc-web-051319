0f8a45c33c70bb171a3a516b4c8a0f9ae3d8ad3e
Dan Kagan <dankagan7@gmail.com>
2019-04-24 23:17:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,8 +68,10 @@ end
 
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, items|
+    
     #binding.pry
-    holiday
+    end
   end
 end
 
