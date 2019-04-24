339cc9d989cbb96a1b61d9f96493ff48c11a9ffc
Dan Kagan <dankagan7@gmail.com>
2019-04-24 23:33:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday, items|
-      binding.pry
+      #binding.pry
     if holiday.include?("BBQ")
       puts items
     end
