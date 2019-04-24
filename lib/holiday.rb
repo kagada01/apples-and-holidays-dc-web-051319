0b4f7d17efce6d2e5355f18dc2ce2f811673465e
Dan Kagan <dankagan7@gmail.com>
2019-04-24 22:34:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,3 +1,4 @@
+
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
@@ -22,48 +23,50 @@ holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  
-  holiday_hash.each do |season, holiday|
-      if season == :winter
-        #binding.pry
-        [:winter]["#{holiday}"] << supply
-      end
-        :winter
+  holiday_hash[:winter].each do |season, holiday|
+    holiday_hash[:winter][season] << supply
   end
-  
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-
+  holiday_hash[:spring].each do |season, holiday|
+    holiday_hash[:spring][season] << supply
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
+  
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-
+  
+  storage = holiday_hash[:winter].map do |season, items|
+    items
+  end
+  storage.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-
+  
+  holiday_hash.each do |season, holiday|
+    binding.pry
+    
+    puts season.to_s.capitalize! + ": "
+    
+    holiday.each do |holiday, items|
+      
+      items = items.join(", ")
+      
+      holidays = holiday.to_s.capitalize!
+     
+     puts holidays + ": " + items
+      
+    end
+  end 
 end
 
 def all_holidays_with_bbq(holiday_hash)
