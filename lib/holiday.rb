a5fe4239eacad983710d6e1de14c4e8fa8ffd64b
Dan Kagan <dankagan7@gmail.com>
2019-04-24 23:39:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,7 +73,7 @@ def all_holidays_with_bbq(holiday_hash)
     holidays.collect do |holiday, items|
       #binding.pry
       if items.include?("BBQ")
-        holiday
+        holiday.compact
       end
     end
   end 
