3ac2d4a9a7a0db22f016a9126c0133beefd1cdff
Dan Kagan <dankagan7@gmail.com>
2019-04-24 23:21:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, items|
       if items.include?("BBQ") == true
-        puts items
+        storage.push(items)
       else
     #binding.pry
       end
